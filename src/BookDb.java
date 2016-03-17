5a41c1efeedf3932c346e0f4171e1d5ef38bf44c
oracle <oracle@GBTC440R04RH.montgomerycollege.edu>
2016-03-17 17:30:42 -0400

    connected to db with books table

--- src/BookDb.java
+++ src/BookDb.java
@@ -1,55 +1,44 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BookDb {
 	public static Book getBookFromDb(String _sku)
 	{
-		Book book;
+		Book book = new Book();
 		
-
-		switch(_sku)
-		{
-			case ("Java1001"):
-			{
-				book = new Book("Java1001", "Head First Java", "Kathy Sierra and Bert Betes", 
-						"Easy to read Java workbook",47.50, true);
-				break;
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql = "select * from books where sku='"+ _sku+"'";
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				book.setSku(_sku);
+				book.setAuthor(rs.getString("author"));
+				book.setDescription(rs.getString("description"));
+				book.setTitle(rs.getString("title"));
+				book.setPrice(rs.getDouble("price"));
 			}
-			case "Java1002":
-			{
-				book = new Book("Java1002", "Thinking in Java", "Bruce Eckel", 
-						"Details about Java under the hood",20.00, true);
-				break;
-			}
-			case "Orcl1003":
-			{
-				book = new Book("Orcl1003", "OCP: Oracle Certified Professional Java SE", "Jeanne Boyarsky", 
-						"Everything you need to know in one place",45.00, true);
-				break;
-			}
-			case "Python1004":
-			{
-				book = new Book("Python1004", "Automate the boring stuff with Python", "Al Sweigert", 
-						"Fun with Python",10.50, true);
-				break;
-			}
-			case "Zombie1005":
-			{
-				book = new Book("Zombie1005", "The Maker's guid to the Zombie apocalypse", "Simon Monk", 
-						"Defend your base with simple circuits, Arduino and Raspberry Pi",16.50, true);
-				break;
-			}
-			case "Rasp1006":
-			{
-				book = new Book("Rasp1006", "Raspberry Pi Projects for evil Genius", "Donald Norris", 
-						"A dozen friendishly fun projects for Rasberry Pi",14.75, true);
-				break;
-			}
-			default:
-			{
-				book = new Book();
-				break;
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				con.close();
+			}catch(SQLException e){
+				e.printStackTrace();
 			}
 		}
-		
+
 		return book;
 	}
 }
