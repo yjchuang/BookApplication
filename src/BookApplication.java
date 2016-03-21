0108269a19eefdf80a2a650efe260bdb1b77f6d3
oracle <oracle@GBTC440R04RH.montgomerycollege.edu>
2016-03-21 15:51:09 -0400

    connect to DB version

--- src/BookApplication.java
+++ src/BookApplication.java
@@ -6,18 +6,36 @@ public class BookApplication {
 	{	
 		Scanner sc = new Scanner(System.in);
 		String _sku = "";
+
+		Book _book;
+
+		Book newBook = new Book("XBPKI30829", "My Father's Dragon", "Julie Chuang", "A very interesting book", 29.95, true);
+		BookDb.updateBookToDb(newBook);
 		
-		System.out.print("Please enter the SKU: ");
-		_sku = sc.next();
-		
-		Book _book = BookDb.getBookFromDb(_sku);
+		_book = BookDb.getBookFromDb("XBPKI30829");
 		System.out.println("This book's SKU is: " + _book.getSku());
 		System.out.printf("Author: %s;\nTitle: %s;\nDescription: %s;\nPrice: %.2f\n", 
 				_book.getAuthor(), _book.getTitle(), _book.getDescription(), _book.getPrice());	
 		
+		BookDb.deleteBookToDb("XBPKI30829");
+		_book = BookDb.getBookFromDb("XBPKI30829");
+		System.out.println("This book's SKU is: " + _book.getSku());
+		System.out.printf("Author: %s;\nTitle: %s;\nDescription: %s;\nPrice: %.2f\n", 
+				_book.getAuthor(), _book.getTitle(), _book.getDescription(), _book.getPrice());	
+		
+		//		_book = BookDb.getBookFromDb("SQMOL88755");
+
+		//		System.out.print("Please enter the SKU: ");
+		//		_sku = sc.next();
+		//		
+		//		Book _book = BookDb.getBookFromDb(_sku);
+		System.out.println("This book's SKU is: " + _book.getSku());
+		System.out.printf("Author: %s;\nTitle: %s;\nDescription: %s;\nPrice: %.2f\n", 
+				_book.getAuthor(), _book.getTitle(), _book.getDescription(), _book.getPrice());	
+
 		sc.close();
 	}
-	
+
 	public static double getBookPrice(Book _myBook)
 	{
 		if(_myBook.getIsInStock() == true)
--- src/BookDb.java
+++ src/BookDb.java
@@ -1,21 +1,25 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class BookDb {
+	static Connection con = null;
+	
+
 	public static Book getBookFromDb(String _sku)
 	{
-		Book book = new Book();
-		
-		Connection con = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		String sql = "select * from books where sku='"+ _sku+"'";
+		
+		Book book = new Book();
+
+		String sql = "select * from books where sku='" + _sku + "'";
+		
 		try{
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
+			con = connectToBooksDb();
 			stmt = con.createStatement();
 			rs = stmt.executeQuery(sql);
 			while(rs.next()){
@@ -26,8 +30,6 @@ public class BookDb {
 				book.setPrice(rs.getDouble("price"));
 			}
 		}catch (SQLException e) {
-			e.printStackTrace();
-		}catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} finally {
 			try {
@@ -40,5 +42,121 @@ public class BookDb {
 		}
 
 		return book;
+	}
+
+	public static void insertBookToDb(Book _book)
+	{
+		int count=0;
+
+		String update_sql = "insert into Books (sku, title, author, description, price) values (?, ?, ?, ?, ?)";
+		try{
+			con = connectToBooksDb();
+
+			PreparedStatement pstmt = con.prepareStatement(update_sql);
+
+			pstmt.setString(1, _book.getSku());
+			pstmt.setString(2, _book.getTitle());
+			pstmt.setString(3, _book.getAuthor());
+			pstmt.setString(4, _book.getDescription());
+			pstmt.setDouble(5, _book.getPrice());
+
+
+			count = pstmt.executeUpdate();
+			if (count == 0)
+				System.out.println("No record found");;
+
+		}catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		finally {
+			try {
+				//rs.close();
+				//stmt.close();
+				con.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void updateBookToDb(Book _book)
+	{
+		int count=0;
+
+		String update_sql = "update Books SET title = ?, author= ?, description = ?, price = ? WHERE sku = ?";
+
+		try{
+			con = connectToBooksDb();
+
+			PreparedStatement pstmt = con.prepareStatement(update_sql);
+			pstmt.setString(1, _book.getTitle());
+			pstmt.setString(2, _book.getAuthor());
+			pstmt.setString(3, _book.getDescription());
+			pstmt.setDouble(4, _book.getPrice());
+			pstmt.setString(5, _book.getSku());
+
+			count = pstmt.executeUpdate();
+			if (count == 0)
+				System.out.println("No record found");;
+
+		}catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				con.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+
+	public static void deleteBookToDb(String _sku)
+	{
+
+		String delete_sql = "delete from Books WHERE sku = ?";
+		try{
+			con = connectToBooksDb();
+
+			PreparedStatement pstmt = con.prepareStatement(delete_sql);
+			pstmt.setString(1, _sku);
+			int count = pstmt.executeUpdate();
+			if (count == 0)
+				System.out.println("No record found");;
+
+		}catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				con.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static Connection connectToBooksDb()
+	{
+	
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:ora1/ora1@localhost:1521:orcl");
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+
+	
+		return con;
 	}
 }
